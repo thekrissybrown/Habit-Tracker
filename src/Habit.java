7e42625f5d36fbe936bbc2ba3a37ca0c6b8091e4
Naleisa <michal.broniek@gmail.com>
2025-04-22 11:24:39 -0400

    Habit.java base started

--- src/Habit.java
+++ src/Habit.java
@@ -1,5 +1,32 @@
 // Placeholder class
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Habit {
 
+    // Initializing variables
+    private String name;
+    private String description;
+    private HabitCategory category;
+    private LocalDate creationDate;
+    private ArrayList<LocalDate> completionDates;
+    private int currentStreak;
+    private int longestStreak;
+
+    // Constructor for adding new habits
+    public Habit(String name, String description, HabitCategory category){
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.creationDate = LocalDate.now();
+        this.completionDates = new ArrayList<>();
+        this.currentStreak = 0;
+        this.longestStreak = 0;
+    }
+
+    // Method to add completion to dates without date provided
+    public void markCompleteNow(){
+        completionDates.add(LocalDate.now());
+    }
 }
