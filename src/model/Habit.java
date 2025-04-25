b6d8a5489efcbfc6e4db80c687ad31013124b434
Naleisa <mbroniek@iu.edu>
2025-04-25 08:47:54 -0400

    Updates to Habit and HabitCategory

--- src/model/Habit.java
+++ src/model/Habit.java
@@ -1,46 +1,44 @@
-// Placeholder class
+package model;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Habit {
-
-    // Initializing variables
     private String name;
     private String description;
     private HabitCategory category;
     private LocalDate creationDate;
     private ArrayList<LocalDate> completionDates;
-    private LocalDate lastCompletionDate;
-    private int currentStreak;
-    private int longestStreak;
 
-    // Constructor for adding new habits
+    // Constructor
     public Habit(String name, String description, HabitCategory category){
         this.name = name;
         this.description = description;
         this.category = category;
         this.creationDate = LocalDate.now();
-        this.lastCompletionDate = null;
-        this.completionDates = new ArrayList<LocalDate>();
-        this.currentStreak = 0;
-        this.longestStreak = 0;
+        this.completionDates = new ArrayList<>();
     }
 
-    // Method to add completion to dates without date provided
-    public void markCompleteNow(){
-        completionDates.add(LocalDate.now());
+    // Getters for Habits
+    public String getName(){
+        return name;
     }
 
-    // Method to update streaks
-    public void updateStreak(){
-        // First completion of a habit will simply do this
-        if (lastCompletionDate == null){
-            currentStreak = 1;
-        } else{
-            
-        }
-
+    public String getDescription(){
+        return description;
     }
+
+    public HabitCategory getCategory(){
+        return category;
+    }
+
+    public LocalDate getCreationDate(){
+        return creationDate;
+    }
+
+    public ArrayList<LocalDate> getCompletionDates(){
+        return completionDates;
+    }
+
 
 }
--- src/model/HabitCategory.java
+++ src/model/HabitCategory.java
@@ -1,5 +1,26 @@
-// Placeholder class
+package model;
 
 public class HabitCategory {
+    private String name;
+    private String description;
 
+    // Constructor
+    public HabitCategory(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters for Habit Category
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString(){
+        return "Category " + name + " " + description;
+    }
 }
