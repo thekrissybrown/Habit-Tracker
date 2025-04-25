e290e6fa14cd17acdbfd741da6c27a90d6f8f137
SaoudAlmajmaie <mustafa.saoud5@gmail.com>
2025-04-25 19:23:37 -0400

    Add files via upload

---
+++ src/DataManager.java
@@ -0,0 +1,31 @@
+// Project Name: Habit Tracker System
+// Author: Mustafa Almajmaie 
+// Date: 2025-04-25
+// Description: This file contains stubs for two classes -   
+		DataManager handles data management, such as reading and writing habit data in a file.
+//               
+//             
+
+
+package controller; 
+
+import java.util.ArrayList;
+
+
+ public class DataManager {
+
+    /* 
+     * Saves the list of habits to a file.
+     */
+    public static void saveHabitsToFile(ArrayList<Habit> habits, String filename) {
+        // To be implemented
+    }
+
+    /* 
+     * Loads a list of habits from a file.
+     */
+    public static ArrayList<Habit> loadHabitsFromFile(String filename) {
+        // To be implemented
+        return null;
+    }
+}
